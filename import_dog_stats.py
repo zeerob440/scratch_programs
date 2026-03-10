c8e90cb4f486c766ac982a281130f168df04ebcf
MU-TH-ER <zeerob440@tutanota.com>
2026-03-10 18:44:50 -0400

    feat: import_dog_stats.py| declared embellished_height var to change the value of zs.height

--- import_dog_stats.py
+++ import_dog_stats.py
@@ -8,3 +8,8 @@ print('height is', zs.height)
 # printing method using dunder __dict__
 print('Height is also', zs.__dict__['height'])
 print(zs.dog_stats)
+
+# attempt to change value of zs.height
+embellished_height = zs.__dict__['height'] = 6969
+
+print(embellished_height)
