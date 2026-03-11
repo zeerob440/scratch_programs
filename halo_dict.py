fefb8f981b7f184c8eeb0e29f2ab272df4f1744d
MU-TH-ER <zeerob440@tutanota.com>
2026-03-11 18:54:07 -0400

    debug: halo_dict.py| line 6 NameError, master_chief is not defined.

--- halo_dict.py
+++ halo_dict.py
@@ -1,11 +1,18 @@
-master_chief: dict = {
-    'name': 'John',
-    'op_id': 117,
-    'ai': 'Cortana',
-    'ship': 'UNSC Pillar of Autumn'
-}
+# place function above dict.
 
 def spartan_id(who, roster_num):
     print(f"I'm {who['name']} {roster_num}")
 
 spartan_id(master_chief, "Finish this fight.")
+
+
+
+master_chief: dict = {
+    'name': 'John',
+    'op_id': 117,
+    'ai': 'Cortana',
+    'ship': 'UNSC Pillar of Autumn',
+    'spartan_id': spartan_id
+}
+
+master_chief['spartan_id'](master_chief, 'Here I am.')
