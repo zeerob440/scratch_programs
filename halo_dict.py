47c55c17b13530a867abbd6d8b19f59ff8c1c903
MU-TH-ER <zeerob440@tutanota.com>
2026-03-11 18:35:46 -0400

    docs: halo_dict.py declared, dict master_chief declared, function spartan_id declared.

---
+++ halo_dict.py
@@ -0,0 +1,11 @@
+master_chief: dict = {
+    'name': 'John',
+    'op_id': 117,
+    'ai': 'Cortana',
+    'ship': 'UNSC Pillar of Autumn'
+}
+
+def spartan_id(who, roster_num):
+    print(f"I'm {who['name']} {roster_num}")
+
+spartan_id(master_chief, "Finish this fight.")
